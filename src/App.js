edf0ece62bd3367a3d50a0d9c0a8914f4be63aab
vijethx <38877972+vijethx@users.noreply.github.com>
2022-06-23 15:42:50 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
 export default function App() {
+	const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
-  return (
-    <div className="mainContainer">
+	return (
+		<div className='mainContainer'>
+			<div className='dataContainer'>
+				<div className='header'>
+					hey there! gm
+					<span role='img' aria-label='wave'>
+						👋
+					</span>
+				</div>
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+				<div className='bio'>I'm Vijeth and I build web apps!</div>
+				<div className='bio'> Connect your Ethereum wallet and wave at me!</div>
 
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
-      </div>
-    </div>
-  );
+				<button className='waveButton' onClick={wave}>
+					Wave at Me
+				</button>
+			</div>
+		</div>
+	);
 }
